f6bcc7dc9bc6ca478e1712d46e1102a1d2b38348
xardasVB <besarabvitaliy@gmail.com>
2020-10-03 15:16:42 +0300

    first commit

---
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour {
+  public float speed = 700;
+  public float sprintSpeed = 1200;
+  Vector2 velocity;
+  private Rigidbody rg;
+
+  void Start() {
+    rg = GetComponent<Rigidbody>();
+  }
+
+  void Update() {
+    velocity.y = Input.GetAxis("Vertical");
+    velocity.x = Input.GetAxis("Horizontal");
+    velocity = velocity.normalized * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed) * Time.deltaTime;
+
+    rg.velocity = transform.forward * velocity.y + transform.right * velocity.x;
+    //transform.Translate(velocity.x, 0, velocity.y);
+  }
+}
